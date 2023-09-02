b701b1c61c1aa4aabcf2abe1d07182476ccfe893
iosaf <116690709+Luisa-GVR@users.noreply.github.com>
2023-09-01 20:01:23 -0800

    Update AgregarVendedor.java

--- src/unison/AgregarVendedor.java
+++ src/unison/AgregarVendedor.java
@@ -1,7 +1,82 @@
 package unison;
 
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class AgregarVendedor {
+    public static void main(String[] args) throws ParseException, IOException {
+        Scanner sc = new Scanner(System.in);
+        final String dataPath = "ArchivosDeAccesoAleatorio-master/src/vendors-data.dat";
+        final String csvPath = "ArchivosDeAccesoAleatorio-master/src/vendors.csv";
+
+        System.out.println("Inserta el nombre");
+        String nombreNuevo = sc.nextLine();
+        System.out.println("Inserta la fecha como dd/mm/aaaa");
+        String fecha = sc.nextLine();
+        System.out.println("Inserta el nombre de la zona");
+        String ciudad = sc.nextLine();
+        System.out.println("Inserta las ventas mensuales");
+        int ventaMensual = sc.nextInt();
+        sc.close();
+
+        Vendor fechaCreator = new Vendor();
+
+        fechaCreator.setFecha(fecha);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = dateFormat.parse(fecha);
+
+        long fechaPasar = date.getTime(); // convertir fecha en long
+
+        BufferedReader length = new BufferedReader(new FileReader(csvPath));
+        int tamanio = 0;
+
+        while (length.readLine() != null) {
+            tamanio++;
+        }
+        length.close();
+        int veCodven = tamanio;
 
 
+        //escribir en el csv
 
+        RandomAccessFile randomAccessFile = new RandomAccessFile(csvPath, "rw");
+        randomAccessFile.seek(randomAccessFile.length());
+        String dataToAppend = "\n" + veCodven + "," + nombreNuevo + "," + fecha + "," + ciudad + "," + ventaMensual;
+        randomAccessFile.writeBytes(dataToAppend);
+        randomAccessFile.close();
+
+
+        //escribir en el dat
+
+        BufferedReader csvFile = null;
+        RandomAccessFile datFile = null;
+        try {
+            csvFile = new BufferedReader(new FileReader(csvPath));
+
+            // archivo binario
+            datFile = new RandomAccessFile(dataPath, "rws");
+
+            String record = null;
+            byte buffer[] = null;
+
+            csvFile.readLine();
+
+            long time = System.currentTimeMillis();
+
+            while ((record = csvFile.readLine()) != null) {
+                datFile.writeInt(veCodven);// numero
+                datFile.write(nombreNuevo.getBytes()); // nombre
+                datFile.writeLong(fechaPasar); //fecha
+                datFile.write(ciudad.getBytes()); // zona
+                datFile.writeInt(ventaMensual); //mensual
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
